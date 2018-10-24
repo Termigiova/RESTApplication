261a0363ed2d40617094f1c645aa4d25ff049302
Termigiova <termigiova@me.com>
2018-10-24 17:17:14 -0500

    Create MovieDetail controller

---
+++ src/main/java/com/controllers/MovieDetailController.java
@@ -0,0 +1,36 @@
+package com.controllers;
+
+import com.models.MovieDetail;
+import com.repositories.MovieDetailRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class MovieDetailController {
+
+    @Autowired
+    MovieDetailRepository movieDetailRepository;
+
+    @RequestMapping(method = RequestMethod.GET, value = "/movieDetails")
+    public Iterable<MovieDetail> movieDetail() { return movieDetailRepository.findAll(); }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/movieDetails")
+    public String save(@RequestBody MovieDetail movieDetail) {
+        movieDetailRepository.save(movieDetail);
+
+        return movieDetail.getId();
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/movieDetails/{id}")
+    public MovieDetail show(@PathVariable String id) { return movieDetailRepository.findById(id).get(); }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/movieDetails/{id}")
+    public MovieDetail update(@PathVariable String id, @RequestBody MovieDetail requestedMovieDetail) {
+        MovieDetail movieDetail = movieDetailRepository.findById(id).get();
+        movieDetail.assignValuesFrom(requestedMovieDetail);
+
+        movieDetailRepository.save(movieDetail);
+        return movieDetail;
+    }
+
+}
