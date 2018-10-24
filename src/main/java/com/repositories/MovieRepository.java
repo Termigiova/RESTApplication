2070e951665678380d12bc4b4d5be45184dbb73f
Termigiova <termigiova@me.com>
2018-10-24 12:35:06 -0500

    Add MovieRepository

---
+++ src/main/java/com/repositories/MovieRepository.java
@@ -0,0 +1,16 @@
+package com.repositories;
+
+import com.models.Movie;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface MovieRepository extends MongoRepository<Movie, String> {
+
+
+    @Override
+    Optional<Movie> findById(String id);
+
+    @Override
+    void delete(Movie deleted);
+}
